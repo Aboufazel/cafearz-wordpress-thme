430b51e049ad764927420d2f2aafe6d1b11c4f95
FrontEnd <a.ab2521@gmail.com>
2023-05-07 14:50:55 +0330

    feat: header menu add done

--- menu.php
+++ menu.php
@@ -1,12 +1,4 @@
 
-<div class="payment-nav-menu">
-    <div class="name">وبلاگ کافه ارز </div>
+<div class="flex flex-col items-start font-semibold lg:text-[14px]">
     <?php wp_nav_menu( array( 'theme_location' => 'sidebar-menu', 'container' =>'', 'menu_class' =>'top-nav' ) ); ?>
-    <button id="m-toggle" class="m-toggle">
-        <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" width="24px" height="24px"><path d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"/></svg>
-    </button>
-    <div class="clearfix"></div>
-    <div class="search-btn" onclick="show_mask('mask_search')">
-        <svg fill="#000000" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="50px" height="50px"><path d="M 21 3 C 11.601563 3 4 10.601563 4 20 C 4 29.398438 11.601563 37 21 37 C 24.355469 37 27.460938 36.015625 30.09375 34.34375 L 42.375 46.625 L 46.625 42.375 L 34.5 30.28125 C 36.679688 27.421875 38 23.878906 38 20 C 38 10.601563 30.398438 3 21 3 Z M 21 7 C 28.199219 7 34 12.800781 34 20 C 34 27.199219 28.199219 33 21 33 C 13.800781 33 8 27.199219 8 20 C 8 12.800781 13.800781 7 21 7 Z"/></svg>
-    </div>
 </div>
